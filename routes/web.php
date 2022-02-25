96e5e059390ad93e0aac72d6f165af6c866af92a
oleg <zitov01@mail.ru>
2022-02-25 21:44:34 +0300

    test commit 10

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@
 use Laravel\Lumen\Routing\Router;
 
 $router->get('/', function () use ($router) {
-    return 45;
+    return 46;
 //    return \App\Models\User::all(); hello
 //    return $router->app->version(); hi lol
 });
